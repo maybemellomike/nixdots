0ac5ac06b12dd89fceee8c77579030ac99236a23
maybemellomike <ubuntu9786@gmail.com>
2026-02-21 10:45:50 -0500

    conf files linked with home manager

--- home.nix
+++ home.nix
@@ -5,6 +5,26 @@
      username = "liam";
      homeDirectory = "/home/liam";
      stateVersion = "25.05"; # match your NixOS version
+     file.".config/waybar" = {
+       source = ./config/waybar;
+       recursive = true;
+     };
+     file.".config/hypr" = {
+       source = ./config/hypr;
+       recursive = true;
+     };
+     file.".config/ghostty" = {
+       source = ./config/ghostty;
+       recursive = true;
+     };
+     file.".config/qutebrowser" = {
+       source = ./config/qutebrowser;
+       recursive = true;
+     };
+     file.".config/yazi" = {
+       source = ./config/yazi;
+       recursive = true;
+     };
   };
 
 }
