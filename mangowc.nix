554eb38e8220c0bdf16d5206e6d4434217d359b8
maybemellomike <ubuntu9786@gmail.com>
2026-03-01 18:24:21 -0500

    mangowc updates

--- mangowc.nix
+++ mangowc.nix
@@ -26,20 +26,63 @@ blur_params_brightness=0.9
 blur_params_contrast=0.9
 blur_params_saturation=1.2
 
+#shadows
+shadows = 1
+layer_shadows = 1
+shadow_only_floating = 1
+shadows_size = 10
+shadows_blur = 15
+shadows_position_x = 0
+shadows_position_y = 0
 
-border_radius=10
+border_radius=5
 no_radius_when_single=0
 no_border_when_single=0
 borderpx=0
 focused_opacity=1.0
-unfocused_opacity=0.9
+unfocused_opacity=0.8
 
 mod = SUPER
 terminal = kitty
 
 exec-once = waybar
 exec-once = waypaper --restore
+exec-once = hypridle
 mouse_natural_scrolling=0
+
+# Animation Configuration(support type:zoom,slide)
+# tag_animation_direction: 1-horizontal,0-vertical
+animations=1
+layer_animations=1
+animation_type_open=slide
+animation_type_close=slide
+animation_fade_in=1
+animation_fade_out=1
+tag_animation_direction=1
+zoom_initial_ratio=0.3
+zoom_end_ratio=0.8
+fadein_begin_opacity=0.5
+fadeout_begin_opacity=0.8
+animation_duration_move=500
+animation_duration_open=400
+animation_duration_tag=350
+animation_duration_close=800
+animation_duration_focus=0
+animation_curve_open=0.2,0.8,0.2,1
+animation_curve_move=0.2,0.8,0.2,1
+animation_curve_tag=0.2,0.8,0.2,1
+animation_curve_close=0.2,0.8,0.2,1
+animation_curve_focus=0.2,0.8,0.2,1
+animation_curve_opafadeout=0.2,0.8,0.2,1
+animation_curve_opafadein=0.2,0.8,0.2,1
+# Scroller Layout Setting
+scroller_structs=20
+scroller_default_proportion=0.8
+scroller_focus_center=0
+scroller_prefer_center=0
+edge_scroller_pointer_focus=1
+scroller_default_proportion_single=1.0
+scroller_proportion_preset=0.5,0.8,1.0
 
 # Keybindings
 bind=SUPER+SHIFT,r,reload_config
